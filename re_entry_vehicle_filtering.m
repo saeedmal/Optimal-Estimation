7bf954f59fb082e8a852a1fd9d9fab61c7ce523b
Saeed Maleki <31936087+saeedmal@users.noreply.github.com>
2019-10-07 15:41:07 -0400

    Update re_entry_vehicle_filtering.m

--- re_entry_vehicle_filtering.m
+++ re_entry_vehicle_filtering.m
@@ -1,10 +1,11 @@
-
 %% Main procedure to generate measurements
 clc
 clear
 close all
 
 global R0 rv muk
+
+R0=6374;
 
 time=200;%simulation time
 deltat=0.05;%measurement sample time
@@ -34,7 +35,7 @@ save('measurement.mat','state','y');
 
 disp('Measurement generation done');
 
-%% Extended Kalman Filter(EKF)
+% Extended Kalman Filter(EKF)
 load measurement %load measurement information
  
 jk=2;
@@ -87,158 +88,28 @@ for i=1:(time/deltat)
 
 end
 
-
 figure
 t=[deltat:deltat:time]; %time vector
 real_measurement=state(:,rind:rind:end);
 plot(t,xe(1,:)-real_measurement(1,:),'linewidth',2);
 hold on
-plot(t,3*sqrt(Pe(rind:rind:end,:)),'r');%,t,-3*sqrt(Pe(rind:rind:end,:)),'r','linewidth',3);
+plot(t,3*sqrt(Pe(1,:)),'Linewidth',3);
+hold on
+plot(t,-3*sqrt(Pe(1,:)),'Linewidth',3);
 rmse=rms(xe(1,:)-real_measurement(1,:));
 title(['error for the 1st state, RMSE= ',num2str(rmse)]);
-grid on;
-
+grid on; 
 disp('Done with EKF')
-keyboard
-%% This section implements UKF 
-for jk=1:8
-    
-    load measurement
-    
-    dt=0.05;
-    dtm=dt*jk;
-    Q=diag([2.4064e-5,2.4064e-5,1e-6])*dtm;%process noise Q
-    
-    L=5;        %number of states                        
-    m=2;        %size of each measurement vector  
-    
-    %scaling factors                                 
-    ki=0;                                                                           
-    s=zeros(L,2*L+1);
-    rv=[0.001 0;0 0.017];
-    musim=xhat0;
-    psim=phat0;
-    
-    options=odeset('AbsTol',1e-7,'RelTol',1e-7);
-    for i=1:(time/dtm)
-    
-        [u,uw]=sigmap(musim,psim);%xsim and psim will be updated one time for each i
-        
-        for  j=1:(2*L+1) 
-            [T,Xk]=ode45(@fdot4,[i*dtm dtm*(i+1)],u(:,j),options);
-            s(:,j)=(Xk(end,:))';%save sigma points for each i:x(k+1|k)
-        end
-        
-        %generating Wi for mean and covariance
-        muk=zeros(L,1);yhatk=zeros(m,1);
-        pxx=zeros(L,L);
-        pxy=zeros(L,m);
-        pyy=zeros(m,m);
-    
-        for k=1:(2*L+1)
-                muk=muk+uw(k)*s(:,k);
-                yhatk=yhatk+uw(k)*out(s(:,k));
-        end
-        
-        for kk=1:(2*L+1)
-          pxx=pxx+uw(kk)*(s(:,kk)-muk)*(s(:,kk)-muk)';
-          pxy=pxy+uw(kk)*(s(:,kk)-muk)*(out(s(:,kk))-yhatk)';
-          pyy=pyy+uw(kk)*(out(s(:,kk))-yhatk)*(out(s(:,kk))-yhatk)';
-        end
-        
-        pyy=pyy+rv;
-        
-        gg=[zeros(1,3);zeros(1,3);eye(3)];
-        gQg=gg*Q*gg';
-        pxx=pxx+gQg; 
-        %updating mukplus1 and sigkplus1 using measurment
-        K=pxy*inv(pyy);%kalman gain
-        rind=round(dtm/dt);
-        yk=(y(rind*i,:))';%measurement
-        musim=muk+K*(yk-yhatk); %update for mean using measurement musim=mu(k+1|k+1)
-        psim=pxx-K*pxy'; %updating covariance using measurement
-        xe(:,i)=musim; %save mean
-        Pe(:,i)=diag(psim); %save covariance
-    end
+
+%output function y=h(x)+measurement noise  
+function h=out(b)
+global R0
+h(1,1)=((b(1,1)-R0)^2+b(2,1)^2)^0.5;
+h(2,1)=atan(b(2,1)/(b(1,1)-R0));
 end
 
-%% This section is the algorithm for CUT4
-musim=xhat0;
-psim=phat0;
-options=odeset('AbsTol',1e-7,'RelTol',1e-7);
-for i=1:(time/dtm)
-    [u,uw]=sigmap(musim,psim);%xsim and psim will be updated one time for each i
-    
-    n=size(u,2);
-    for  j=1:n
-        [T,Xk]=ode45(@fdot4,[i*dtm dtm*(i+1)],u(:,j),options);
-        s(:,j)=(Xk(end,:))';%save sigma points for each i:x(k+1|k)
-    end
-    %generating Wi for mean and covariance
-    muk=zeros(L,1);yhatk=zeros(m,1);
-    pxx=zeros(L,L);
-    pxy=zeros(L,m);
-    pyy=zeros(m,m);
-    for k=1:(n)
-        muk=muk+uw(k)*s(:,k);
-        yhatk=yhatk+uw(k)*out(s(:,k));
-    end
-    for kk=1:(n)
-        pxx=pxx+uw(kk)*(s(:,kk)-muk)*(s(:,kk)-muk)';
-        pxy=pxy+uw(kk)*(s(:,kk)-muk)*(out(s(:,kk))-yhatk)';
-        pyy=pyy+uw(kk)*(out(s(:,kk))-yhatk)*(out(s(:,kk))-yhatk)';
-    end
-    
-    pyy=pyy+rv;
-    gg=[zeros(1,3);zeros(1,3);eye(3)];
-    gQg=gg*Q*gg';
-    
-    pxx=pxx+gQg; %propagate
-    
-    %updating mukplus1 and sigkplus1 using measurment
-    K=pxy*inv(pyy);%kalman gain
-    rind=round(dtm/dt);
-    yk=(y(rind*i,:))';%measurement
-    musim=muk+K*(yk-yhatk); %update for mean using measurement musim=mu(k+1|k+1)
-    psim=pxx-K*pxy'; %updating covariance using measurement
-    
-    xe(:,i)=musim; %save mean
-    Pe(:,i)=diag(psim); %save covariance
-end
-
-t=[dtm:dtm:time]; %time vector
-
-state=state(:,rind:rind:end);
-
-subplot(3,2,1);plot(t,xe(1,:)-state(1,:),'linewidth',2);
-hold on
-plot(t,3*sqrt(Pe(1,:)),'r',t,-3*sqrt(Pe(1,:)),'r','linewidth',3);
-title('error for the 1st state');
-rmse(1)=rms(xe(1,:)-state(1,:));
-grid on;
-
-%re-entry model state space
-function xdot=fdot4(t,x)
-%model parameters
-beta0=0.59783;
-R0=6374;
-Gm0=3.986e5;
-H0=13.406;
-R=(x(1)^2+x(2)^2)^0.5;
-G=-Gm0/(R^3);
-V=(x(3)^2+x(4)^2)^0.5;
-beta=beta0*exp(x(5));
-D=-beta*V*exp((R0-R)/H0);
-%model state space
-xdot(1,1)=x(3);
-xdot(2,1)=x(4);
-xdot(3,1)=D*x(3)+G*x(1);
-xdot(4,1)=D*x(4)+G*x(2);
-xdot(5,1)=0;
-end
-
-%re-entry model state space
-function xdot=fdot5(t,x)
+%linearization of the process dynamics at each measurement step
+function xdot=fdot5(t,x) 
 global R0 rv muk
 %model parameters
 beta0=0.59783;
@@ -299,34 +170,4 @@ xdot(11:15,1)=xdot2(:,2);
 xdot(16:20,1)=xdot2(:,3);
 xdot(21:25,1)=xdot2(:,4);
 xdot(26:30,1)=xdot2(:,5);
-end
-
-%generating sigma points
-function [X,xw]=sigmap(mex1,covx)
-
-    L=length(mex1);
-    mex=reshape(mex1,[L,1]);
-    ki=0;
-    A=sqrtm((L+ki)*covx);
-    
-    X=zeros(L,2*L+1);
-    X(:,1)=mex;
-
-    xw(1)=ki/(L+ki);
-
-    for ii=2:(L+1)
-        X(:,ii)=mex+A(:,ii-1);
-        xw(ii)=1/(2*(L+ki));
-    end    
-    for ii=(L+2):(2*L+1)
-        X(:,ii)=mex-A(:,ii-L-1);
-        xw(ii)=1/(2*(L+ki));
-    end  
-end
-
-%output function    
-function h=out(b)
-global R0
-h(1,1)=((b(1,1)-R0)^2+b(2,1)^2)^0.5;
-h(2,1)=atan(b(2,1)/(b(1,1)-R0));
 end
